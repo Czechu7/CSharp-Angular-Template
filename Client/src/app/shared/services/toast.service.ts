2644b9e2cc334b6e2d9069cbbd6b996232fb70ce
michalrusak <michal1406@onet.pl>
2025-03-01 19:28:50 +0100

    FE: add toast component and service

---
+++ Client/src/app/shared/services/toast.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import type { Keys, Severity } from '../types/toast.types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ToastService {
+  constructor(private messageService: MessageService) {}
+
+  show(
+    severity: Severity,
+    summary: string,
+    detail: string,
+    key?: Keys,
+    life?: number
+  ) {
+    this.messageService.add({
+      severity,
+      summary,
+      detail,
+      key,
+      life,
+    });
+  }
+
+  showSuccess(summary: string, detail: string, key?: Keys, life?: number) {
+    this.show('success', summary, detail, key, life);
+  }
+
+  showInfo(summary: string, detail: string, key?: Keys, life?: number) {
+    this.show('info', summary, detail, key, life);
+  }
+
+  showWarning(summary: string, detail: string, key?: Keys, life?: number) {
+    this.show('warn', summary, detail, key, life);
+  }
+
+  showError(summary: string, detail: string, key?: Keys, life?: number) {
+    this.show('error', summary, detail, key, life);
+  }
+
+  clear(key?: string) {
+    this.messageService.clear(key);
+  }
+}
