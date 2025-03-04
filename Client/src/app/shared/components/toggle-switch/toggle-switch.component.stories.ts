1cf86d0cfc9e95a738736f1bca8f37b65fa11d28
michalrusak <michal1406@onet.pl>
2025-03-04 12:54:25 +0100

    FE: add toggle switch

---
+++ Client/src/app/shared/components/toggle-switch/toggle-switch.component.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { ToggleSwitchComponent } from './toggle-switch.component';
+import { ToggleSwitchProps } from '../../types/toogleSwitch.types';
+
+const meta: Meta<ToggleSwitchComponent> = {
+  title: 'Components/ToggleSwitchComponent',
+  component: ToggleSwitchComponent,
+};
+export default meta;
+
+const args: ToggleSwitchProps = {
+  formControlName: 'toggle',
+  disabled: false,
+  iconOn: 'pi-check',
+  iconOff: 'pi-times',
+  invalid: false,
+};
+
+type ToggleSwitchStory = StoryObj<ToggleSwitchComponent>;
+
+export const primary: ToggleSwitchStory = {
+  args: args,
+};
---
+++ Client/src/app/shared/types/toogleSwitch.types.ts
@@ -0,0 +1,7 @@
+export interface ToggleSwitchProps {
+  formControlName: string | null;
+  disabled: boolean;
+  iconOn?: string;
+  iconOff?: string;
+  invalid: boolean;
+}
