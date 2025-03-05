df55516752c6266651097ea5ba2721da21e89b92
michalrusak <michal1406@onet.pl>
2025-03-05 18:32:19 +0100

    FE: add form service

---
+++ Client/src/app/shared/services/form.service.ts
@@ -0,0 +1,134 @@
+import { Injectable } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  PasswordsForm,
+  LoginForm,
+  RegisterForm,
+  PasswdRecoveryForm,
+} from '../models/form.model';
+import { equivalentValidator } from '../validators/equivalent.validator';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FormService {
+  constructor() {}
+
+  initPasswordsForm(): FormGroup<PasswordsForm> {
+    return new FormGroup(
+      {
+        password: new FormControl('', {
+          validators: [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.maxLength(75),
+          ],
+          nonNullable: true,
+        }),
+        repeatedPassword: new FormControl('', {
+          validators: [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.maxLength(75),
+          ],
+          nonNullable: true,
+        }),
+      },
+      { validators: [equivalentValidator('password', 'repeatedPassword')] },
+    );
+  }
+
+  initLoginForm(): FormGroup<LoginForm> {
+    return new FormGroup({
+      login: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(50),
+        ],
+        nonNullable: true,
+      }),
+      password: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(75),
+        ],
+        nonNullable: true,
+      }),
+    });
+  }
+
+  initRegisterForm(): FormGroup<RegisterForm> {
+    return new FormGroup({
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email],
+        nonNullable: true,
+      }),
+      login: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(50),
+        ],
+        nonNullable: true,
+      }),
+      password: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(75),
+        ],
+        nonNullable: true,
+      }),
+      repeatedPassword: new FormControl('', {
+        validators: [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(75),
+        ],
+        nonNullable: true,
+      }),
+    });
+  }
+
+  initPasswdRecoveryForm(): FormGroup<PasswdRecoveryForm> {
+    return new FormGroup({
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email],
+        nonNullable: true,
+      }),
+    });
+  }
+
+  getErrorMessage(control: FormControl): string {
+    if (control.hasError('required')) {
+      return 'Ta kontrolka jest wymagana.';
+    }
+
+    if (
+      control.hasError('pattern') &&
+      control.errors?.['pattern']?.['requiredPattern'] === '/^\\d{2}-\\d{3}$/'
+    ) {
+      return 'Podano kod pocztowy w niepoprawnym formacie.';
+    }
+
+    if (control.hasError('minlength')) {
+      return `Minimalna ilość znaków: ${control.errors?.['minlength']?.requiredLength}.`;
+    }
+
+    if (control.hasError('maxlength')) {
+      return `Maksymalna ilość znaków: ${control.errors?.['maxlength']?.requiredLength}.`;
+    }
+
+    if (control.hasError('email')) {
+      return `Niepoprawny adres e-mail.`;
+    }
+
+    if (control.hasError('passwordsNotEqual')) {
+      return 'Hasła muszą być takie same.';
+    }
+
+    return '';
+  }
+}
