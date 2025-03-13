ed75becad199d266dc1d0e3f318b907baa9aadca
kkolcz <k.kolcz@outlook.com>
2025-03-13 01:50:14 +0100

    FE: extend BaseResponse model to include success status, status code, pagination, and error details

--- Client/src/app/core/_models/base-response.model.ts
+++ Client/src/app/core/_models/base-response.model.ts
@@ -1,4 +1,14 @@
 export interface BaseResponse<T> {
+  success: boolean;
+  statusCode: number;
   message: string;
   data: T;
+  pagination?: IPagination;
+  errors?: string[];
+}
+
+interface IPagination {
+  pageSize: number;
+  pageNumber: number;
+  totalCount: number;
 }
