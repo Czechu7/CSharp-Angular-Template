55c0d5db69c2c2c1a13af394a54a70609fcc7e7b
michalrusak <michal1406@onet.pl>
2025-03-27 18:28:12 +0100

    FE: add form initialization and validation configurations for login, registration, and password recovery

--- Client/src/app/shared/services/form.service.ts
+++ Client/src/app/shared/services/form.service.ts
@@ -1,132 +1,39 @@
-import { inject, Injectable } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { TranslateService } from '@ngx-translate/core';
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import {
-  PasswordsForm,
   LoginForm,
-  RegisterForm,
   PasswdRecoveryForm,
+  PasswordsForm,
+  RegisterForm,
   ThemeForm,
 } from '../models/form.model';
-import { equivalentValidator } from '../validators/equivalent.validator';
+import { initLoginForm } from './forms/initLoginForm';
+import { initPasswdRecoveryForm } from './forms/initPasswdRecoveryForm';
+import { initPasswordsForm } from './forms/initPasswordsForm';
+import { initRegisterForm } from './forms/initRegisterForm';
+import { initThemeForm } from './forms/initThemeForm';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FormService {
-  private translateService = inject(TranslateService);
-
-  initPasswordsForm(): FormGroup<PasswordsForm> {
-    return new FormGroup(
-      {
-        password: new FormControl('', {
-          validators: [Validators.required, Validators.minLength(8), Validators.maxLength(75)],
-          nonNullable: true,
-        }),
-        repeatedPassword: new FormControl('', {
-          validators: [Validators.required, Validators.minLength(8), Validators.maxLength(75)],
-          nonNullable: true,
-        }),
-      },
-      { validators: [equivalentValidator('password', 'repeatedPassword')] }
-    );
+  public getPasswordsForm(): FormGroup<PasswordsForm> {
+    return initPasswordsForm();
   }
 
-  initLoginForm(): FormGroup<LoginForm> {
-    return new FormGroup({
-      email: new FormControl('', {
-        validators: [
-          Validators.required,
-          Validators.email,
-          Validators.minLength(3),
-          Validators.maxLength(50),
-        ],
-        nonNullable: true,
-      }),
-      password: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(8), Validators.maxLength(75)],
-        nonNullable: true,
-      }),
-    });
+  public getLoginForm(): FormGroup<LoginForm> {
+    return initLoginForm();
   }
 
-  initRegisterForm(): FormGroup<RegisterForm> {
-    return new FormGroup({
-      username: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(3), Validators.maxLength(50)],
-        nonNullable: true,
-      }),
-      firstName: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(3), Validators.maxLength(50)],
-        nonNullable: true,
-      }),
-      lastName: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(3), Validators.maxLength(50)],
-        nonNullable: true,
-      }),
-      email: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(3), Validators.maxLength(50)],
-        nonNullable: true,
-      }),
-      password: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(8), Validators.maxLength(75)],
-        nonNullable: true,
-      }),
-      confirmPassword: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(8), Validators.maxLength(75)],
-        nonNullable: true,
-      }),
-    });
+  public getRegisterForm(): FormGroup<RegisterForm> {
+    return initRegisterForm();
   }
 
-  initThemeForm(): FormGroup<ThemeForm> {
-    return new FormGroup({
-      theme: new FormControl(false, {
-        validators: [Validators.required],
-        nonNullable: true,
-      }),
-    });
+  public getThemeForm(): FormGroup<ThemeForm> {
+    return initThemeForm();
   }
 
-  initPasswdRecoveryForm(): FormGroup<PasswdRecoveryForm> {
-    return new FormGroup({
-      email: new FormControl('', {
-        validators: [Validators.required, Validators.email],
-        nonNullable: true,
-      }),
-    });
-  }
-
-  getErrorMessage(control: FormControl): string {
-    if (control.hasError('required')) {
-      return this.translateService.instant('ERRORS.REQUIRED');
-    }
-
-    if (
-      control.hasError('pattern') &&
-      control.errors?.['pattern']?.['requiredPattern'] === '/^\\d{2}-\\d{3}$/'
-    ) {
-      return this.translateService.instant('ERRORS.POSTAL_CODE_FORMAT');
-    }
-
-    if (control.hasError('minlength')) {
-      const minLength = control.errors?.['minlength']?.requiredLength;
-      return this.translateService.instant('ERRORS.MIN_LENGTH', { minLength });
-    }
-
-    if (control.hasError('maxlength')) {
-      const maxLength = control.errors?.['maxlength']?.requiredLength;
-      return this.translateService.instant('ERRORS.MAX_LENGTH', { maxLength });
-    }
-
-    if (control.hasError('email')) {
-      return this.translateService.instant('ERRORS.INVALID_EMAIL');
-    }
-
-    if (control.hasError('passwordsNotEqual')) {
-      return this.translateService.instant('ERRORS.PASSWORDS_NOT_EQUAL');
-    }
-
-    return '';
+  public getPasswdRecoveryForm(): FormGroup<PasswdRecoveryForm> {
+    return initPasswdRecoveryForm();
   }
 }
