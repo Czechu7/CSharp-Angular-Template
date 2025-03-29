83a2d561b96cb62d448822cb5daf1f785304ca06
kkolcz <k.kolcz@outlook.com>
2025-03-29 22:27:45 +0100

    FE: add role guard for route authorization

---
+++ Client/src/app/core/_guards/role.guard.ts
@@ -0,0 +1,9 @@
+import { inject } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { RoleService } from '../_services/role/role.service';
+
+export const roleGuard: CanActivateFn = (route, state) => {
+  const roleService = inject(RoleService);
+  const requiredRole = route.data['roles'] as string;
+  return roleService.isAuthorized(requiredRole);
+};
