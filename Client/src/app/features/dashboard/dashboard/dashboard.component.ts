7cc1191a617b7803f3be00369ca16db254be0017
kkolcz <k.kolcz@outlook.com>
2025-03-30 17:17:52 +0200

    FE: add directives isLoggedIn and notLoggedIn and examples in dashboard

--- Client/src/app/features/dashboard/dashboard/dashboard.component.ts
+++ Client/src/app/features/dashboard/dashboard/dashboard.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { HasRoleDirective } from '../../../shared/directives/has-role.directive';
+import { IsLoggedInDirective } from '../../../shared/directives/is-logged-in.directive';
+import { NotLoggedInDirective } from '../../../shared/directives/not-logged-in.directive';
 import { RolesEnum } from '../../../enums/roles.enum';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [TranslateModule, HasRoleDirective],
+  imports: [TranslateModule, HasRoleDirective, IsLoggedInDirective, NotLoggedInDirective],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
