9b120053dcae9a21bc012b1d8378194930fd119a
kkolcz <k.kolcz@outlook.com>
2025-03-30 01:59:43 +0100

    FE: simplify role guard logic by removing type assertion

--- Client/src/app/core/_guards/role.guard.ts
+++ Client/src/app/core/_guards/role.guard.ts
@@ -2,8 +2,10 @@ import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { RoleService } from '../_services/role/role.service';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const roleGuard: CanActivateFn = (route, state) => {
   const roleService = inject(RoleService);
-  const requiredRole = route.data['roles'] as Array<string>;
+  const requiredRole = route.data['roles'];
+
   return roleService.isAuthorized(requiredRole);
 };
