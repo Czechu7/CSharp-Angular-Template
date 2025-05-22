55c952bca6e023a67d501b2e9e66cc973ec11d02
Czechu7 <choocas0@gmail.com>
2025-05-22 20:15:05 +0200

    refactor: remove validation attributes from Role property in UpdateUserProfileDto

--- API/Application/CQRS/AdminPanel/DTOs/UpdateUserProfileDto.cs
+++ API/Application/CQRS/AdminPanel/DTOs/UpdateUserProfileDto.cs
@@ -7,9 +7,6 @@ namespace Application.CQRS.AdminPanel.DTOs;
 public class UpdateUserProfileDto : IMapBidirectional<User>
 {
     public string? Username { get; set; }
-    
-    [Required(ErrorMessage = "Role is required")]
-    [StringLength(50, MinimumLength = 2, ErrorMessage = "Role must be between 2 and 50 characters")]
     public string? Role { get; set; }
     public string? Email { get; set; }
     public string? FirstName { get; set; }
