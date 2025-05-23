5279d5d110f6076c31b5089e9e6d6bdea9964e89
Czechu7 <choocas0@gmail.com>
2025-05-23 15:02:27 +0200

    feat: rename Role property to Roles in UpdateUserProfileDto and update related handler logic

--- API/Application/CQRS/AdminPanel/DTOs/UpdateUserProfileDto.cs
+++ API/Application/CQRS/AdminPanel/DTOs/UpdateUserProfileDto.cs
@@ -7,7 +7,7 @@ namespace Application.CQRS.AdminPanel.DTOs;
 public class UpdateUserProfileDto : IMapBidirectional<User>
 {
     public string? Username { get; set; }
-    public string? Role { get; set; }
+    public string? Roles { get; set; }
     public string? Email { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
